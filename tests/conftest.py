6584c3df11077510bef46baf130df987bf183ccc
Wanji <wanji.zhu@eifer.uni-karlsruhe.de>
2020-02-23 13:09:26 +0100

    add unit test for comments

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,10 +2,24 @@ import pytest
 from rest_framework.test import APIClient
 from rest_framework.test import APIRequestFactory
 
+from .factories import UserFactory
+
 
 @pytest.fixture(scope='module')
 def drf_client():
     _client = APIClient()
+    yield _client
+
+
+@pytest.fixture()
+def drf_auth_client(db):
+    _client = APIClient()
+
+    # attached a new user to client
+    _client.user = UserFactory.create(username='auth-user', password="password")
+
+    _client.credentials(HTTP_AUTHORIZATION='Token ' + _client.user.token)
+
     yield _client
 
 
